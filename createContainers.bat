fecc1592dfe49aaf0c10b2e213a90e19fa09445f
vbalan <victor.balan93@gmail.com>
2015-07-22 17:55:04 +0300

    unit test
     * java unit test core container

--- createContainers.bat
+++ createContainers.bat
@@ -3,3 +3,4 @@ docker build -t coduno/fingerprint-c ./c
 docker build -t coduno/fingerprint-cpp ./cpp
 docker build -t coduno/fingerprint-java ./java
 docker build -t coduno/fingerprint-py ./py
+docker build -t coduno/fingerprint-javaut ./javaut
